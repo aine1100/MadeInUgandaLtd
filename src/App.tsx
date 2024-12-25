972709c35dbf2ae6d5ffa2a42dcefa6bc1ff13ba
aine1100 <ainedushimire@gmail.com>
2024-12-25 18:53:57 +0200

    adjst deply

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,23 @@
-import About from "./pages/about";
-import Home from "./pages/home"
+import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import About from "./pages/about";
+import Home from "./pages/home";
 import Services from "./pages/Service";
 import Products from "./pages/Product";
 import Contact from "./pages/contact";
-export default function App() {
+
+const App: React.FC = () => {
   return (
-    <>
     <Router>
       <Routes>
-        <Route path="/" element={<Home/>}/>
-        <Route path="/about" element={<About/>}/>
-        <Route path="/service" element={<Services/>}/>
-        <Route path="/products" element={<Products/>}/>
-        <Route path="/contact" element={<Contact/>}/>
-
-
-
-
-
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/service" element={<Services />} />
+        <Route path="/products" element={<Products />} />
+        <Route path="/contact" element={<Contact />} />
       </Routes>
     </Router>
-    </>
-  )
-}
+  );
+};
+
+export default App;
